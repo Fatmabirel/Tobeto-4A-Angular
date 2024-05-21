d09eee5e844c9f63a000b3b3073fd12814dde598
Fatma Birel <fatmabrl11@gmail.com>
2024-05-21 16:34:49 +0300

    filter query params

--- src/app/features/categories/components/category-list-group/category-list-group.component.ts
+++ src/app/features/categories/components/category-list-group/category-list-group.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { CategoryListItem } from '../../models/category-list-item';
 import { ListGroupComponent, ListGroupItem, ListGroupItems } from '../../../../shared/components/list-group/list-group.component';
 
@@ -12,6 +12,10 @@ import { ListGroupComponent, ListGroupItem, ListGroupItems } from '../../../../s
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CategoryListGroupComponent {
+
+  @Input() initialSelectedCategoryId?: number | null;
+  @Output() changeSelect = new EventEmitter<number | null>();
+  
   categoryList: CategoryListItem[] = [
     { id: 1, name: 'Beverages' },
     { id: 2, name: 'Canned Goods' },
@@ -22,7 +26,7 @@ export class CategoryListGroupComponent {
     { id: 7, name: 'Snacks' },
   ]; // Mock data
 
-  @Output() changeSelect = new EventEmitter<number | null>();
+
 
   onChangeSelect(selectedItemId: string | null) {
     this.changeSelect.emit(Number(selectedItemId));
--- src/app/routes/home-page/home-page.component.ts
+++ src/app/routes/home-page/home-page.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { BasicLayoutComponent } from "../../shared/components/basic-layout/basic-layout.component";
 import { CategoryListGroupComponent } from '../../features/categories/components/category-list-group/category-list-group.component';
 import { ProductCartListComponent } from '../../features/products/product-cart-list/product-cart-list.component';
@@ -24,5 +24,22 @@ export class HomePageComponent {
   
     onChangeCategorySelect(event: number | null) {
       this.selectedCategoryId = event;
+      this.router.navigate([], {
+        queryParams: { categoryId: this.selectedCategoryId },
+        relativeTo: this.route,
+      });
     }
+
+    constructor(private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.getProductFiltersFromRoute();
+  }
+
+  getProductFiltersFromRoute() {
+    this.route.queryParams.subscribe((queryParams) => {
+      const categoryId = queryParams['categoryId'];
+      if (categoryId) this.selectedCategoryId = Number(categoryId);
+    }).unsubscribe();
+  }
   }
