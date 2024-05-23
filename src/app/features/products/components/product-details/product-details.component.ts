102d9aa8f41f41fe6f72cdfd8c95284191ae6dd7
Fatma Birel <fatmabrl11@gmail.com>
2024-05-23 21:15:50 +0300

    appWelcome directive added

--- src/app/features/products/components/product-details/product-details.component.ts
+++ src/app/features/products/components/product-details/product-details.component.ts
@@ -12,6 +12,7 @@ import { ButtonComponent } from '../../../../shared/components/button/button.com
 import { VatPipe } from '../../pipes/vat.pipe';
 import { HighlightDirective } from '../../directives/highlight.directive';
 import { AppButtonDirective } from '../../directives/appButton.directive';
+import { AppWelcomeDirective } from '../../directives/appWelcome.directive';
 
 
 @Component({
@@ -24,6 +25,7 @@ import { AppButtonDirective } from '../../directives/appButton.directive';
     VatPipe,
     AppButtonDirective,
     HighlightDirective,
+    AppWelcomeDirective
   ],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss',
