c445a0122c3108d5615e43214278588970b41511
Fatma Birel <fatmabrl11@gmail.com>
2024-05-24 22:19:38 +0300

    guard operations

--- src/app/routes/auth/auth.routes.ts
+++ src/app/routes/auth/auth.routes.ts
@@ -1,9 +1,22 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { AdminPanelPageComponent } from './admin-panel-page/admin-panel-page.component';
+import { securedRouteGuard } from '../../../core/auth/guards/secured-route.guard';
+import { MyAccountPageComponent } from './my-account-page/my-account-page.component';
 
 export const authRoutes: Routes = [
   {
     path: 'auth/login', // localhost:4200/auth/login
     component: LoginPageComponent,
+  },
+  {
+    path: 'admin-panel',
+    canActivate: [securedRouteGuard],
+    component: AdminPanelPageComponent,
+  },
+  {
+    path: 'my-account',
+    canActivate: [securedRouteGuard],
+    component: MyAccountPageComponent,
   },
 ];
